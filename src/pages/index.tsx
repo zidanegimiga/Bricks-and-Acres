a30696f71eb5600e14d10acc16b0d37d518d77ee
zidanegimiga <zidanegimiga@gmail.com>
2022-07-28 00:16:40 +0300

    styles: configure scss color variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from 'next/head';
+
 export default function Index() {
   return (
     <div>
+      <Head>
+        <title>Home - Bricks & Acres</title>
+      </Head>
     </div>
   );
 }
