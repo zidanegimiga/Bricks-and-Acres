7649b87cafb89fc161a88235da3c8881709033e0
zidanegimiga <zidanegimiga@gmail.com>
2022-07-27 23:40:33 +0300

    config: add fonts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -22,6 +22,9 @@ class MyDocument extends Document {
             key="og-title"
           />
           <link rel="icon" href="/logo.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&family=Raleway:wght@400;700&family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
